e11216a3925ce1f3d220d45e7b7bf2f7308217c5
Zulu <jzuluaga55@gmail.com>
2017-05-25 10:50:07 -0500

    Lab 01 fisnish

---
+++ Lab01/Employee.cs
@@ -0,0 +1,23 @@
+﻿namespace Lab01
+{
+    public class Employee
+    {
+        #region Properties
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public Date BornDate { get; set; }
+
+        public Date HireDate { get; set; }
+        #endregion
+
+        #region Methods
+        public override string ToString()
+        {
+            return string.Format("{0} {1}\n\tBorn date...: {2}\n\tHire date...: {3}", 
+                                   FirstName, LastName, BornDate, HireDate);
+        }
+        #endregion
+    }
+}
---
+++ Lab01/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var employees = new List<Employee>();
+
+            employees.Add(new Employee
+            {
+                BornDate = new Date(1974, 9, 23),
+                FirstName = "Juan",
+                HireDate = new Date(2017, 5, 9),
+                LastName = "Zuluaga",
+            });
+
+            employees.Add(new Employee
+            {
+                BornDate = new Date(1954, 13, 43),
+                FirstName = "Celia",
+                HireDate = new Date(2000, 2, 29),
+                LastName = "Cruz",
+            });
+
+            employees.Add(new Employee
+            {
+                BornDate = new Date(1984, 12, 12),
+                FirstName = "Freddy",
+                HireDate = new Date(2001, 2, 29),
+                LastName = "Mercury",
+            });
+
+            foreach (var employee in employees)
+            {
+                Console.WriteLine(employee);
+            }
+
+            Console.WriteLine("Press any key to finish...");
+            Console.ReadKey();
+            Console.WriteLine("Hasta la vista baby");
+        }
+    }
+}
